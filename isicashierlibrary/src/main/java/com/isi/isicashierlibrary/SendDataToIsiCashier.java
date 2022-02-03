167dc98ea1a41e7a46dc4851e7fdf91fbed5ff73
edoardo <edoardogullo@libero.it>
2022-02-03 09:12:33 +0100

    isi v2

--- isicashierlibrary/src/main/java/com/isi/isicashierlibrary/SendDataToIsiCashier.java
+++ isicashierlibrary/src/main/java/com/isi/isicashierlibrary/SendDataToIsiCashier.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import androidx.fragment.app.Fragment;
 
 import com.google.gson.Gson;
+import com.isi.isiapi.classes.IsiCashBillAndElements;
 
 public class SendDataToIsiCashier {
 
-    public static void sendBill(Bill bill, Activity activity){
+    public static void sendBill(IsiCashBillAndElements bill, Activity activity){
 
         Intent myIntent = new Intent();
         myIntent.setClassName("com.isi.isicashier", "com.isi.isicashier.MainActivity");
@@ -21,7 +22,7 @@ public class SendDataToIsiCashier {
 
     }
 
-    public static void sendBill(Bill bill, Fragment activity){
+    public static void sendBill(IsiCashBillAndElements bill, Fragment activity){
 
         Intent myIntent = new Intent();
         myIntent.setClassName("com.isi.isicashier", "com.isi.isicashier.MainActivity");
@@ -38,18 +39,5 @@ public class SendDataToIsiCashier {
         myIntent.setClassName("com.isi.isicashier", "com.isi.isicashier.printerSettings.PrintersActivity");
         activity.startActivityForResult(myIntent, RequestCode.reqCode);
     }
-
-    public static void sendProcutsNoUI(Bill bill, Activity activity, Payment.PAYMENT_TYPE paymentType){
-
-        Intent myIntent = new Intent();
-        myIntent.setClassName("com.isi.isicashier", "com.isi.isicashier.invisibleActivity.PaymentAutoActivity");
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(bill.getProducts());
-        myIntent.putExtra("data", jsonString);
-        myIntent.putExtra("payment_type", paymentType.toString());
-        activity.startActivityForResult(myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION), RequestCode.reqCode);
-
-    }
-
 
 }
